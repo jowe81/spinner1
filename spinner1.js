7d076df5c158d1f88532362ae87fa4e28c899a47
Johannes Weber <johannes@drweber.de>
2021-11-06 14:07:04 -0700

    Implementation of one round

--- spinner1.js
+++ spinner1.js
@@ -2,15 +2,29 @@ setTimeout(() => {
   process.stdout.write('\r|   ');
 }, 100);
 
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
 
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
 
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   ');
+// }, 700);
+
+
+const spin = ['\r|   ', '\r/   ', '\r-   ', '\r\\   '];
+
+for (let i = 0; i < spin.length * 2; i++) {
+  setTimeout(() => {
+    process.stdout.write(spin[i % spin.length]);
+  }, i * 200);
+}
+
+//End with a pipe
 setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
+  process.stdout.write('\r|\n');
+}, 1700);
